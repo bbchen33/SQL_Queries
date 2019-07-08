01ca29b8865e955cdb941cc7a06a91dae0d78a49
bbchen33 <52116103+bbchen33@users.noreply.github.com>
2019-07-08 12:46:12 -0600

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,11 @@
+---Users by country---
+SELECT DATE_TRUNC('day', e.occurred_at),
+       location,
+       COUNT(*) 
+FROM tutorial.yammer_events e
+JOIN tutorial.yammer_users u
+ON e.user_id = u.user_id
+AND e.occurred_at >= '2014-05-01'
+AND e.occurred_at < '2014-09-01'
+GROUP BY 1,2
+ORDER BY 1,2
